028e83067b376356abf22ff6671c964c1c39b163
MaykeBolt <maykesouza2805@gmail.com>
2025-01-25 20:24:47 -0300

    feat: Fazendo botões radiais customizados e o tema em celulares/tablets.

--- src/app/Presentation/View/components/CadastroForm.tsx
+++ src/app/Presentation/View/components/CadastroForm.tsx
@@ -8,7 +8,7 @@ export default function CadastroForm() {
         <div className="bg-white cadastroform relative font-mono">
             {/* bg-green-950 */}
             <div className="pt-14 pb-14">
-                <div className="bg-gray-300 w-28 h-28 imgcontainer">
+                <div className="bg-gray-50 w-28 h-28 imgcontainer">
                     <img src="../../../../../assets/images/Rooftops_Emerald_Ring_Racers_(Bigger).png" width="150"
                     height="auto" />
                 </div>
@@ -25,14 +25,14 @@ export default function CadastroForm() {
             <div>
                 <form>
                     <div className="inline-flex space-x-[10vw] text-gray-600 text-lg containerleftpadding">
-                        <div className="inline-flex space-x-2">
-                            <input type="radio" name="role"/>
-                            <p>Professor</p>
+                        <div className="inline-flex tb:space-x-2">
+                            <input type="radio" id="teacher" name="role" className="radiobtn"/>
+                            <label htmlFor="teacher">Professor</label>
                         </div>
 
-                        <div className="inline-flex space-x-2">
-                            <input type="radio" name="role"/>
-                            <p>Aluno</p>
+                        <div className="inline-flex tb:space-x-2 max-tb:space-x-10">
+                            <input type="radio" id="student" name="role" className="radiobtn"/>
+                            <label htmlFor="student">Aluno</label>
                         </div>
                     </div>
 
@@ -40,40 +40,42 @@ export default function CadastroForm() {
                     <div className="pt-2 pb-2 text-center">
                         <div className="">
                             <input type="text" className="textinput nameinput" placeholder="Nome *"/>
-                            <br className="nameinputbreak mb-4"/>
+                            <br className="nameinputbreak mb-8"/>
                             <input type="text" className="textinput nameinput" placeholder="Sobrenome *"/>
-                          <br className="mb-4"/>
+                          <br className="mb-8"/>
                         </div>                          
 
                         <div>
                             <input type="email" className="textinput otherinputs" placeholder="Preencha com seu Email *"/>
-                              <br className="mb-4"/>
+                              <br className="mb-8"/>
                             <input type="password" className="textinput otherinputs" placeholder="Senha *"/>
-                              <br className="mb-4" />
+                              <br className="mb-8" />
                             <input type="tel" className="textinput otherinputs" placeholder="Telefone *"/>
                         </div>
                     </div>
 
                     {/* bg-green-100 */}
-                    <div className=" inline-flex space-x-[3vw] mt-5 text-gray-600 text-lg containerleftpadding">
-                        <p>Gênero:</p>
-                        <div className="inline-flex space-x-2">
-                            <input type="radio" name="gender"/>
-                            <p>Masculino</p>
+                    <p className="genderintro810px">Gênero</p>
+                    <div className=" inline-flex tb:space-x-[3vw] max-tb:mt-3 tb:mt-5 text-gray-600 text-lg 
+                    containerleftpadding">
+                    <p className="genderintro">Gênero:</p>
+                        <div className="inline-flex tb:space-x-2">
+                            <input type="radio" id="male" name="gender" className="radiobtn"/>
+                            <label htmlFor="male">Masculino</label>
                         </div>
 
-                        <div className="inline-flex space-x-2">
-                            <input type="radio" name="gender"/>
-                            <p>Feminino</p>
+                        <div className="inline-flex tb:space-x-2 max-tb:space-x-10">
+                            <input type="radio" id="female" name="gender" className="radiobtn"/>
+                            <label htmlFor="female">Feminino</label>
                         </div>
                     </div>
                 </form>
 
-                <button className="confirmbtn hover:bg-yellow-500 mt-20 mb-8 p-2 rounded-3xl block m-auto">
+                <button className="confirmbtn hover:bg-yellow-500 tb:mt-20 max-tb:mt-8 mb-8 p-2 rounded-3xl block m-auto">
                     <div className="inline-flex ml-2 mr-2 align-middle">
                         <img src="../../../../../assets/images/Rooftops_Emerald_Ring_Racers_(Bigger).png" width="50"
                         height="auto" />
-                        <p className="text-gray-600 text-2xl text-center ml-3">Cadastro</p>
+                        <p className="text-gray-600 text-2xl text-center ml-3 font-bold">Cadastro</p>
                     </div>
                 </button>
             </div>
